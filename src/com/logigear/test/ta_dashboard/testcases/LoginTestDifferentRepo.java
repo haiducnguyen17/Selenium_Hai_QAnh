4b0743a522f7687da334d136e386a0fff1d57901
Hai Nguyen <hai.duc.nguyen@logigear.com>
2019-06-18 11:13:10 +0700

    add different repo

--- src/com/logigear/test/ta_dashboard/testcases/LoginTestDifferentRepo.java
+++ src/com/logigear/test/ta_dashboard/testcases/LoginTestDifferentRepo.java
@@ -17,7 +17,9 @@ public class LoginTestDifferentRepo extends BaseTest{
 		String USERNAME = "administrator";
 		String PASSWORD = "";
 		String SAMPLE_REPO = "SampleRepository";
+		String Another_REPO = "Dashboard_NguyenDucHai";
 		
+		//login repo
 		LoginPage loginpage = new LoginPage();
 		HomePage homePage = loginpage.login(USERNAME, PASSWORD, SAMPLE_REPO);
 
@@ -25,6 +27,22 @@ public class LoginTestDifferentRepo extends BaseTest{
 		System.out.println("RepoName is: "+actualMsg);
 		String expectedMsg = "SampleRepository";
 		Assert.assertEquals(actualMsg, expectedMsg, "Can't login with correct credentials");
+		
+		//logout dashboard 
 		homePage.logOut();
+		
+		
+		//login another repo
+		LoginPage loginpage1 = new LoginPage();
+		HomePage homePage1 = loginpage1.login(USERNAME, PASSWORD, Another_REPO);
+		
+		String actualMsg1 = homePage1.getRepoName();
+		System.out.println("Another RepoName is: "+actualMsg1);
+		String expectedMsg1 = "Dashboard_NguyenDucHai";
+		Assert.assertEquals(actualMsg1, expectedMsg1, "Can't login with correct credentials");
+		
+		
+		
+		
 	}
 }
